d8d3e874b99ebb72f9678dcd7b200e82c18e2e6f
papu93 <damian93domin@gmail.com>
2017-10-31 16:45:02 -0300

    Retorno de lista de items
    Signed-off-by: papu93 <damian93domin@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from ScrapyCocoProviders.crawler_run import ScrapyCocoProviders
+import json
 
 product = 'llantas'
 celulares = ScrapyCocoProviders(product)
-celulares.runMercadoLibre()
+celulares.run_mercado_libre()
+print(celulares.get_results())
